9f5cea969a83b78c7b41fc9deafa6573ddaac78d
dzonint <dzonint@yahoo.com>
2020-02-26 18:08:14 +0100

    Add article page

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -10,13 +10,22 @@ use App\Entity\Article;
 class ArticleController extends AbstractController {
 
     /**
-     * @Route("/")
+     * @Route("/", name="article_list")
      * @Method({"GET"})
      */
     public function index() {
         $articles = $this->getDoctrine()->getRepository(Article::class)->findAll();
 
         return $this->render('articles/index.html.twig', ['articles' => $articles]);
+    }
+
+    /**
+     * @Route("/article/{id}", name="article_show")
+     */
+    public function show(int $id) {
+        $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
+
+        return $this->render('articles/show.html.twig', ['article' => $article]);
     }
 }
 
