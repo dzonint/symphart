e3c4fecb7268c8daf586888392d77856dfec6b87
dzonint <dzonint@yahoo.com>
2020-02-26 18:39:15 +0100

    Add article creation

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -1,11 +1,16 @@
 <?php
 namespace App\Controller;
 
+use App\Entity\Article;
+
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-use App\Entity\Article;
 
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 
 class ArticleController extends AbstractController {
 
@@ -17,6 +22,35 @@ class ArticleController extends AbstractController {
         $articles = $this->getDoctrine()->getRepository(Article::class)->findAll();
 
         return $this->render('articles/index.html.twig', ['articles' => $articles]);
+    }
+
+    /**
+     * @Route("/article/new", name="new_article")
+     * @Method({"GET", "POST"})
+     */
+    public function new(Request $request) {
+        $article = new Article();
+
+        $form = $this->createFormBuilder($article)
+        ->add('title', TextType::class, ['attr' => ['class' => 'form-control']])
+        ->add('body', TextAreaType::class, ['required' => false, 'attr' => ['class' => 'form-control']])
+        ->add('save', SubmitType::class, ['label' => 'Create', 'attr' => ['class' => 'btn btn-primary mt-3']])
+        ->getForm();
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+            $article = $form->getData();
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($article);
+            $em->flush();
+
+            return $this->redirectToRoute('article_list');
+        }
+
+        return $this->render('articles/new.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 
     /**
